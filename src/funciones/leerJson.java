3be6f1677a29aeef5d3f67cddd0bb26eeb958508
aalopz02 <aalopz02@gmail.com>
2017-08-21 19:44:55 -0600

    Listo lectura iniciales, falta json

--- src/funciones/leerJson.java
+++ src/funciones/leerJson.java
@@ -7,10 +7,26 @@ import java.util.LinkedList;
 import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class leerJson {
-    private
-    public static void main(String[] args){
+    private static List listaArchivosDisponibles = new leerIniciales().leerArchivos();
 
+    public static void main(String[] args){
+        System.out.println(listaArchivosDisponibles);
+        leerCaracteristicas(listaArchivosDisponibles.indexOf("Persona.json"));
+    }
+    protected static JSONArray leerCaracteristicas(int indice){
+        JSONParser parser = new JSONParser();
+        JSONArray arregloCaracteristicas = new JSONArray();
+        try {
+            Object lectura = parser.parse(new FileReader("./src/datos/"+listaArchivosDisponibles.get(indice)));
+            JSONObject jsonObject = (JSONObject) lectura;
+            arregloCaracteristicas = (JSONArray) jsonObject.get(listaArchivosDisponibles.get(indice).toString().replaceAll(".json",""));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(arregloCaracteristicas);
+        return arregloCaracteristicas;
     }
 }
